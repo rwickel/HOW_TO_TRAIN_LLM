184c7ef4a925492e8e5c1ed312e7cecbfe1b225d
Robert Wickel <robert.wickel@gmx.de>
2025-06-12 10:48:19 +0200

    Update main.py

--- Extract_Text_From_PDF_Adobe/main.py
+++ Extract_Text_From_PDF_Adobe/main.py
@@ -3,8 +3,8 @@ from reader.extract_from_pdf import ExtractTextInfoFromPDF
 if __name__ == "__main__":
     # Example usage:
     # Set env variables or replace with actual values if needed.
-    client_id="a15c19e647c74c89b6837c5f40bbc4c3"
-    client_secret="p8e-Y55EkNWC54xQ_7R-g7w0U_4XGY48UHPE"
+    client_id="a15c1..." # insert your ADOBE ID
+    client_secret="p8..." # insert ADOBE KEY
     
     # Provide path to input PDF and desired output directory
     input_pdf = "resources/ISO_26262-3_746716_EN.pdf"
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     pdf_name = ExtractTextInfoFromPDF.run(input_pdf, output_directory, client_id, client_secret)
     print(f"Processed PDF name: {pdf_name}")
 
-    #input_json = "output/ExtractTextInfoFromPDF/ISO_26262-6_746747_EN.json"
+    
 
 
